fa8dd78a574439bf1e5d35fb1b5b90d7293c7d12
DylanRitchings <43646210+DylanRitchings@users.noreply.github.com>
2021-04-01 12:20:02 +0100

    Update createCluster.sh

--- createCluster.sh
+++ createCluster.sh
@@ -4,7 +4,7 @@ if kubectl config view --- cluster: null
 eksctl create cluster /
 --name TaskCluster /
 --region eu-west-2 /
---nodegroup-name DemoNodes /
+--nodegroup-name TaskCluster /
 --nodes 2 /
 --nodes-min 2 /
 --nodes-max 3 /
