c25c2c25737d9f223e5363e31cbaa6fa54aac59f
DylanRitchings <43646210+DylanRitchings@users.noreply.github.com>
2021-04-01 12:01:00 +0100

    Create createCluster.sh

---
+++ createCluster.sh
@@ -0,0 +1,12 @@
+eksctl create cluster /
+--name MySQLCluster /
+--region eu-west-2 /
+--nodegroup-name DemoNodes /
+--nodes 2 /
+--nodes-min 2 /
+--nodes-max 3 /
+--node-type t3.medium /
+--with-oidc /
+--ssh-access /
+--ssh-public-key /home/ubuntu/.ssh/id_rsa.pub /
+--managed
